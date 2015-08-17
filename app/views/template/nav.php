5ae1e87012b89908cef018f3332e12f7eb89744e
Mubassir Hayat <mubassirhayat@gmail.com>
2015-08-17 16:03:29 +0500

    Navigation edited

--- app/views/template/nav.php
+++ app/views/template/nav.php
@@ -11,10 +11,10 @@
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Home</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Contact</a></li>
-				<li class="dropdown">
+				<!-- <li class="active"><a href=".">Home</a></li> -->
+				<!-- <li><a href="#">About</a></li> -->
+				<!-- <li><a href="#">Contact</a></li> -->
+				<!-- <li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
 					<ul class="dropdown-menu">
 						<li><a href="#">Action</a></li>
@@ -25,12 +25,12 @@
 						<li><a href="#">Separated link</a></li>
 						<li><a href="#">One more separated link</a></li>
 					</ul>
-				</li>
+				</li> -->
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li class="active"><a href="#">Default <span class="sr-only">(current)</span></a></li>
-				<li><a href="#">Static top</a></li>
-				<li><a href="# ">Fixed top</a></li>
+				<li><a href="#">Students</a></li>
+				<li><a href="#">Courses</a></li>
+				<li><a href="#">Teachers</a></li>
 			</ul>
 		</div><!--/.navbar-collapse -->
 	</div>
