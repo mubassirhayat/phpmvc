fd70e979effc9dc7f206769d400ebd4134486d05
Mubassir Hayat <mubassirhayat@gmail.com>
2015-08-18 16:56:48 +0500

    App class updated for error conroller

--- app/lib/App.php
+++ app/lib/App.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* 
+*
 */
 class App
 {
@@ -14,33 +14,40 @@ class App
 	public function __construct()
 	{
 		$url = $this->parseUrl();
-		if (file_exists('../app/controllers/' . $url[0] . '.php')) 
-		{
-			$this->controller = $url[0];
-			unset($url[0]);
+		if (isset($url[0])) {
+			if (file_exists('../app/controllers/' . $url[0] . '.php'))
+			{
+				$this->controller = $url[0];
+				// unset($url[0]);
+			} else {
+				# code...
+				$this->controller = 'error';
+				// unset($url[0]);
+			}
 		}
 
 		require_once '../app/controllers/' . $this->controller . '.php';
-
 		$this->controller = new $this->controller();
 
-		if (isset($url[1])) 
+		if (isset($url[1]))
 		{
-			if (method_exists($this->controller, $url[1])) 
+			if (method_exists($this->controller, $url[1]))
 			{
 				$this->action = $url[1];
-				unset($url[1]);
+				// unset($url[1]);
+			} else {
+				$this->action = 'error404';
+				// unset($url[1]);
 			}
 		}
 
 		$this->parameters = $url ? array_values($url) : [];
-
 		call_user_func_array([$this->controller, $this->action], $this->parameters);
 	}
 
-	protected function parseUrl() 
+	protected function parseUrl()
 	{
-		if (isset($_GET['url'])) 
+		if (isset($_GET['url']))
 		{
 			return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
 		}
